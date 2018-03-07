f1641528adaf03088d74c3774ad4c381e852bf7e
fabulousduck <ryanvlaming@icloud.com>
2018-03-07 21:51:52 +0100

    remove adding opcodes to the emu struct

--- main.c
+++ main.c
@@ -46,21 +46,6 @@ void emulate_cycle(Emu * emu)
 Emu * create_emu()
 {
     Emu * emu = malloc(sizeof(Emu));
-    Opcode opcodes[OPCODE_COUNT] = {
-        {"0NNN", CALL}, {"00E0", DISPLAY}, {"00EE", FLOW},
-        {"1NNN", FLOW}, {"2NNN", FLOW}, {"3XNN", COND},
-        {"4XNN", COND}, {"5XY0", COND}, {"6XNN", CONST},
-        {"7XNN", CONST}, {"8XY0", ASSIGN}, {"8XY1", BITOP},
-        {"8XY2", BITOP}, {"8XY3", BITOP}, {"8XY4", MATH},
-        {"8XY5", MATH}, {"8XY6", BITOP}, {"8XY7", MATH},
-        {"8XYE", BITOP}, {"9XY0", COND}, {"ANNN", MEM},
-        {"BNNN", FLOW}, {"CXNN", RAND}, {"DXYN", DISP},
-        {"EX9E", KEYOP}, {"EXA1", KEYOP}, {"FX07", TIMER},
-        {"FX0A", KEYOP}, {"FX15", TIMER}, {"FX18", SOUND},
-        {"FX1E", MEM}, {"FX29", MEM}, {"FX33", BCD},
-        {"FX55", MEM}, {"FX65", MEM}
-    };
-    memcpy(emu->opcodes, opcodes, OPCODE_COUNT);
     return emu;
 }
 
